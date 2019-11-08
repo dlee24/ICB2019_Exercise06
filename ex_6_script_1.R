6a59db0ded4963a04a77aa240af58a786eec50b7
loganthayer1 <logan.thayer@gmail.com>
2019-11-08 01:43:23 -0500

    Script 1 minor edit, Diane Lee Logan Thayer

--- ex_6_script_1.R
+++ ex_6_script_1.R
@@ -1,8 +1,8 @@
-#works on data sets stored as variables in the global environment
+#works on data set files
 #requires package data.table to be installed.
 #r_head() works the same as head -n in unix to keep a certain number of lines
 #usage: r_head("file_name", number_of_lines)
-#examples: r_head("wages.csv",3) will return the first three rows of data from 
+#example: r_head("wages.csv",3) will return the first three rows of data from 
 library(data.table)
 r_head <- function(x,n){
   data=fread(file=x, fill=TRUE)
